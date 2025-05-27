c42550109541c76d36d7558737f0609d4349f1dc
Nitish-Mashal <nitishmashal0@gmail.com>
2025-05-27 11:15:59 +0530

    Reverted the changes for html code for redirect to WS to DS

--- website_setting_custom_app/public/js/logo_redirect.js
+++ website_setting_custom_app/public/js/logo_redirect.js
@@ -1,18 +1,18 @@
-console.log("redirect from website settings")
-document.addEventListener("DOMContentLoaded", function () {
-    const observer = new MutationObserver(() => {
-        const logo = document.querySelector('header .navbar-brand');
-        if (logo && !logo.dataset.customRedirect) {
-            logo.dataset.customRedirect = 'true';
-            logo.addEventListener('click', function (e) {
-                e.preventDefault();
-                window.location.href = '/app/home'; // redirect to Desk
-            });
-        }
-    });
+// console.log("redirect from website settings")
+// document.addEventListener("DOMContentLoaded", function () {
+//     const observer = new MutationObserver(() => {
+//         const logo = document.querySelector('header .navbar-brand');
+//         if (logo && !logo.dataset.customRedirect) {
+//             logo.dataset.customRedirect = 'true';
+//             logo.addEventListener('click', function (e) {
+//                 e.preventDefault();
+//                 window.location.href = '/app/home'; // redirect to Desk
+//             });
+//         }
+//     });
 
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
-});
+//     observer.observe(document.body, {
+//         childList: true,
+//         subtree: true,
+//     });
+// });
